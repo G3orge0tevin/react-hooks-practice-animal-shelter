dd0cb944c234073e242277d5128f000982d83cf0
Tevin <georgetevin0@gmail.com>
2024-08-29 23:26:06 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,34 @@ import React, { useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
+import data from "./db.json"
+
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (newType) => {
+    setFilters({
+      type: newType,
+    });
+  };
+
+  const onFindPetsClick = () => {
+    const filteredPets = filters.type === "all"
+      ? data.pets
+      : data.pets.filter(pet => pet.type === filters.type);
+  
+    setPets(filteredPets);
+  };
+
+  const onAdoptPet = (id) => {
+    setPets((prevPets) =>
+      prevPets.map((pet) =>
+        pet.id === id ? { ...pet, isAdopted: true } : pet
+      )
+    );
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +39,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
